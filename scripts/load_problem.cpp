25352b368383c646b1813659f41e2094fdc69da3
Qishuai Liu <liuqs1216@gmail.com>
2023-04-13 13:36:02 +0100

    add start point, and generate a query for fixed start and goal point.

--- scripts/load_problem.cpp
+++ scripts/load_problem.cpp
@@ -11,11 +11,57 @@
 #include <robowflex_library/io/visualization.h>
 #include <robowflex_library/scene.h>
 #include <robowflex_library/robot.h>
+#include <robowflex_library/builder.h>
+#include <motion_bench_maker/problem_generator.h>
 
 using namespace robowflex;
 
 int main(int argc, char **argv)
 {
-    std::cout << "test 1111 " << std::endl;
+    // Enables multiple instances running with the same name
+    ros::init(argc, argv, "load_problem");
+    ros::NodeHandle node("~");
+
+    std::string robot_file, scene_file_dir, var_file, request_file, planning_group;
+
+    std::string exec_name = "load_problem";
+
+    // Load ROS params
+    size_t error = 0;
+    // Parsing the parametes from the param server
+    error += !parser::get(exec_name, node, "robot", robot_file);
+    error += !parser::get(exec_name, node, "scene", scene_file_dir);
+    error += !parser::get(exec_name, node, "planning_group", planning_group);
+    parser::shutdownIfError(exec_name, error);
+
+    auto robot = std::make_shared<Robot>("RoboCop");
+    robot->initializeFromYAML(robot_file);
+    auto rviz = std::make_shared<IO::RVIZHelper>(robot);
+
+    auto scene = std::make_shared<Scene>(robot);
+
+    std::vector<std::string> scene_file_vec;
+    for (int i = 0; i < 34; ++i)
+    {
+        scene_file_vec.push_back(scene_file_dir + std::to_string(i) + ".yaml");
+    }
+
+    int count = 0;
+
+    while (true)
+    {
+        count = (count + 1) % 34;
+        scene->fromYAMLFile(scene_file_vec[count]);
+
+        rviz->updateScene(scene);  // auto generate_file_name =
+                                   // generateNewScene(getSceneFolder(file_name));
+        // auto request = std::make_shared<MotionRequestBuilder>(robot);
+        // request->fromYAMLFile(request_file);
+        // request->setConfig("PRM");
+        auto pg = std::make_shared<ProblemGenerator>(request_file);
+        // pg->setParameters();
+        // sampled_scene->toYAMLFile(generate_file_name);
+        parser::waitForUser("Displaying the scene!");
+    }
     return 0;
 }
--- src/scene_sampler.cpp
+++ src/scene_sampler.cpp
@@ -67,11 +67,13 @@ ScenePtr SceneSampler::sample(const SceneConstPtr &nominal) const
     for (const auto &object_var : object_variations_)
         for (const auto &name : object_var->names)
         {
+            ROS_INFO("name is: %s", name.c_str());
             // make sure the first object didnot change.
-            if (name == "Can1")
+            if (name == "Can1" || name == "Can6")
             {
                 continue;
             }
+
             if (not scene->hasObject(name))
             {
                 ROS_WARN("Object %s does not exist in the nominal scene !", name.c_str());
